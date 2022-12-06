bcb1c2a1153ebcd0e1c8c18347db1dfe0405d605
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-12-06 07:05:59 +0200

    Restructurated the road build, ready to add other cars

--- src/lab_m1/tema2/tema2.cpp
+++ src/lab_m1/tema2/tema2.cpp
@@ -10,7 +10,8 @@
 using namespace std;
 using namespace m1;
 
-vector <pair<double, double>> points;
+vector <pair<double, double>> first_road_points;
+vector <pair<double, double>> second_road_points;
 vector<pair<double, double>> trees_points;
 
 struct triangle {
@@ -66,6 +67,10 @@ void Tema2::create_parallelepiped(string name, double point1_x, double point1_z,
 
 }
 
+double distance(pair<double, double> A, pair<double, double> B) {
+	return sqrt((A.first - B.first) * (A.first - B.first) + (A.second - B.second) * (A.second - B.second));
+}
+
 void add_triangle(int x, int y, int z, vector<VertexFormat> vertices) {
 	triangle t;
 	t.A = { vertices[x].position.x, vertices[x].position.z };
@@ -85,39 +90,46 @@ void Tema2::create_road() {
 
 	vector<VertexFormat> vertices;
 	vector<unsigned int> indices;
-	points.push_back({ points[0].first, points[0].second });
-	points.push_back({ points[1].first, points[1].second });
-	int n = points.size();
-	for (int i = 0; i <= n - 4; i += 2) {
+
+	first_road_points.push_back({ first_road_points[0].first, first_road_points[0].second });
+	int n = first_road_points.size();
+
+	for (int i = 0; i < n - 1; ++i) {
+		pair<double, double> direction = { first_road_points[i + 1].first - first_road_points[i].first, first_road_points[i + 1].second - first_road_points[i].second};
+		pair<double, double> product = { -direction.second, direction.first };
+		product.first /= distance(first_road_points[i], first_road_points[i + 1]);
+		product.second /= distance(first_road_points[i], first_road_points[i + 1]);
+
+		pair<double, double> exterior_point = { first_road_points[i].first + 1.5 * product.first, first_road_points[i].second + 1.5 * product.second };
+
+		second_road_points.push_back(exterior_point);
+	}
+	second_road_points.push_back({ second_road_points[0].first, second_road_points[0].second });
+
+
+	for (int i = 0; i < n - 1; ++i) {
 		vertices.clear();
 		indices.clear();
-		vertices.push_back(VertexFormat(glm::vec3(points[i].first, 0.2, points[i].second), glm::vec3(0, 0, 0)));
-		vertices.push_back(VertexFormat(glm::vec3(points[i + 1].first, 0.2, points[i + 1].second), glm::vec3(0, 0, 0)));
-		vertices.push_back(VertexFormat(glm::vec3(points[i + 2].first, 0.2, points[i + 2].second), glm::vec3(0, 0, 0)));
-		vertices.push_back(VertexFormat(glm::vec3(points[i + 3].first, 0.2, points[i + 3].second), glm::vec3(0, 0, 0)));
+		vertices.push_back(VertexFormat(glm::vec3(first_road_points[i].first, 0.2, first_road_points[i].second), glm::vec3(0, 0, 0)));
+		vertices.push_back(VertexFormat(glm::vec3(second_road_points[i].first, 0.2, second_road_points[i].second), glm::vec3(0, 0, 0)));
+		vertices.push_back(VertexFormat(glm::vec3(first_road_points[i + 1].first, 0.2, first_road_points[i + 1].second), glm::vec3(0, 0, 0)));
+		vertices.push_back(VertexFormat(glm::vec3(second_road_points[i + 1].first, 0.2, second_road_points[i + 1].second), glm::vec3(0, 0, 0)));
 
-		if (i >= 1) {
-			vertices.push_back(VertexFormat(glm::vec3(points[i - 1].first, 0.2, points[i - 1].second), glm::vec3(0, 0, 0)));
-			make_triangle_indices(0, 1, 4, indices, vertices);
-			make_triangle_indices(0, 2, 4, indices, vertices);
-			make_triangle_indices(0, 3, 4, indices, vertices);
-			make_triangle_indices(1, 2, 4, indices, vertices);
-			make_triangle_indices(2, 3, 4, indices, vertices);
-		}
 		make_triangle_indices(0, 1, 2, indices, vertices);
+		make_triangle_indices(1, 2, 3, indices, vertices);
 		make_triangle_indices(0, 1, 3, indices, vertices);
 		make_triangle_indices(0, 2, 3, indices, vertices);
-		make_triangle_indices(1, 2, 3, indices, vertices);
 
-		meshes["road_point_" + to_string(i / 2)] = new Mesh("road_point_" + to_string(i / 2));
-		meshes["road_point_" + to_string(i / 2)]->InitFromData(vertices, indices);
+
+		meshes["road_point_" + to_string(i)] = new Mesh("road_point_" + to_string(i));
+		meshes["road_point_" + to_string(i)]->InitFromData(vertices, indices);
 	}
 }
 
 void Tema2::render_road() {
-	int n = points.size();
-	for (int i = 0; i <= n - 4; i += 2) {
-		MyRenderMesh(meshes["road_point_" + to_string(i / 2)], shaders["VertexColor"], glm::mat4(1));
+	int n = first_road_points.size();
+	for (int i = 0; i < n - 1; ++i) {
+		MyRenderMesh(meshes["road_point_" + to_string(i)], shaders["VertexColor"], glm::mat4(1));
 	}
 }
 
@@ -160,42 +172,49 @@ bool check_on_road(double x, double y) {
 	return 0;
 }
 
+double tree_size = 0.25;
+
 void Tema2::create_trees() {
 
-	create_parallelepiped("base", -0.5, 0.5, 0.5, -0.5, 0.2, 7, glm::vec3(0.46, 0.36, 0.28));
-	create_parallelepiped("branch1", -0.25, 0.25, 0.25, -0.25, 0.25, 3, glm::vec3(0.46, 0.36, 0.28));
-	create_parallelepiped("branch2", -0.25, 0.25, 0.25, -0.25, 0.25, 3, glm::vec3(0.46, 0.36, 0.28));
-	create_parallelepiped("main_leaf", -2, 2, 2, -2, 0.2, 3, glm::vec3(0.004, 0.196, 0.125));
-	create_parallelepiped("leaf1", -1, 1, 1, -1, 0.2, 2, glm::vec3(0.004, 0.196, 0.125));
-	create_parallelepiped("leaf2", -1, 1, 1, -1, 0.2, 2, glm::vec3(0.004, 0.196, 0.125));
+	create_parallelepiped("base", -0.5 * tree_size, 0.5 * tree_size, 0.5 * tree_size, -0.5 * tree_size, 0.2 * tree_size, 7 * tree_size, glm::vec3(0.46, 0.36, 0.28));
+	create_parallelepiped("branch1", -0.25 * tree_size, 0.25 * tree_size, 0.25 * tree_size, -0.25 * tree_size, 0.25 * tree_size, 3 * tree_size, glm::vec3(0.46, 0.36, 0.28));
+	create_parallelepiped("branch2", -0.25 * tree_size, 0.25 * tree_size, 0.25 * tree_size, -0.25 * tree_size, 0.25 * tree_size, 3 * tree_size, glm::vec3(0.46, 0.36, 0.28));
+	create_parallelepiped("main_leaf", -2 * tree_size, 2 * tree_size, 2 * tree_size, -2 * tree_size, 0.2 * tree_size, 3 * tree_size, glm::vec3(0.004, 0.196, 0.125));
+	create_parallelepiped("leaf1", -1 * tree_size, 1 * tree_size, 1 * tree_size, -1 * tree_size, 0.2 * tree_size, 2 * tree_size, glm::vec3(0.004, 0.196, 0.125));
+	create_parallelepiped("leaf2", -1 * tree_size, 1 * tree_size, 1 * tree_size, -1 * tree_size, 0.2 * tree_size, 2 * tree_size, glm::vec3(0.004, 0.196, 0.125));
 
-}
-
-double distance(pair<double, double> A, pair<double, double> B) {
-	return sqrt((A.first - B.first) * (A.first - B.first) + (A.second - B.second) * (A.second - B.second));
 }
 
 void find_trees_points() {
 
-	for (int x = -200; x <= 200; ++x) {
-		for (int z = -200; z <= 200; ++z) {
+	for (double x = -50; x <= 35; x += 0.2) {
+		for (double z = -50; z <= 25; z += 0.2) {
 			bool ok = 1;
 			double min_dist = 1e9;
-			for (auto point : points) {
+			for (auto point : first_road_points) {
 				double dist = distance(point, { x, z });
-				if ( dist <= 5) {
+				if ( dist <= 1.5) {
 					ok = 0;
 					break;
 				}
 				min_dist = min(min_dist, dist);
 			}
 
-			if (min_dist > 10) {
+			for (auto point : second_road_points) {
+				double dist = distance(point, { x, z });
+				if (dist <= 1.5) {
+					ok = 0;
+					break;
+				}
+				min_dist = min(min_dist, dist);
+			}
+
+			if (min_dist > 3) {
 				ok = 0;
 			}
 
 			for (auto tree_point : trees_points) {
-				if (distance(tree_point, { x, z }) <= 15) {
+				if (distance(tree_point, { x, z }) <= 6) {
 					ok = 0;
 					break;
 				}
@@ -219,26 +238,26 @@ void Tema2::render_trees() {
 		MyRenderMesh(meshes["base"], shaders["VertexColor"], base_transformation);
 
 		glm::mat4 branch1_transformation = main_transformation;
-		branch1_transformation *= transform3D::Translate(0, 2, 0);
+		branch1_transformation *= transform3D::Translate(0, 2 * tree_size, 0);
 		branch1_transformation *= transform3D::RotateOZ(1);
 		MyRenderMesh(meshes["branch1"], shaders["VertexColor"], branch1_transformation);
 
 		glm::mat4 branch2_transformation = main_transformation;
-		branch2_transformation *= transform3D::Translate(0, 3, 0);
+		branch2_transformation *= transform3D::Translate(0, 3 * tree_size, 0);
 		branch2_transformation *= transform3D::RotateOZ(-1);
 		MyRenderMesh(meshes["branch2"], shaders["VertexColor"], branch2_transformation);
 
 		glm::mat4 main_leaf_transformation = main_transformation;
-		main_leaf_transformation *= transform3D::Translate(0, 6.5, 0);
+		main_leaf_transformation *= transform3D::Translate(0, 6.5 * tree_size, 0);
 		MyRenderMesh(meshes["main_leaf"], shaders["VertexColor"], main_leaf_transformation);
 
 		glm::mat4 leaf1_transformation = main_transformation;
-		leaf1_transformation *= transform3D::Translate(-2, 3.3, 0);
+		leaf1_transformation *= transform3D::Translate(-2 * tree_size, 3.3 * tree_size, 0);
 		leaf1_transformation *= transform3D::RotateOZ(1);
 		MyRenderMesh(meshes["leaf1"], shaders["VertexColor"], leaf1_transformation);
 
 		glm::mat4 leaf2_transformation = main_transformation;
-		leaf2_transformation *= transform3D::Translate(2, 4.3, 0);
+		leaf2_transformation *= transform3D::Translate(2 * tree_size, 4.3 * tree_size, 0);
 		leaf2_transformation *= transform3D::RotateOZ(-1);
 		MyRenderMesh(meshes["leaf2"], shaders["VertexColor"], leaf2_transformation);
 	}
@@ -251,28 +270,24 @@ void Tema2::Init()
 {
 	camera = new Camera();
 	minimap_camera = new Camera();
-	extract_road_points(points);
+	extract_road_points(first_road_points);
 	create_road();
 	create_trees();
 	find_trees_points();
 
-	create_parallelepiped("ground", -1000, 1000, 1000, -1000, 0.1, 0.1, glm::vec3(0, 1, 1));
-	create_parallelepiped("truck", 0, 1, 1, -1, 0.2, 1, glm::vec3(1, 0, 0));
+	create_parallelepiped("ground", -200, 200, 200, -200, 0.1, 0.1, glm::vec3(0, 1, 1));
+	create_parallelepiped("truck", -0.1, 0.2, 0.1, -0.2, 0.2, 0.34, glm::vec3(1, 0, 0));
 
 	translateX = 0;
 	translateY = 0;
 	translateZ = 0;
 
 	initial_camera_X = 0;
-	initial_camera_Y = 3;
-	initial_camera_Z = 4.5f;
-
-	truck_center_X = 0.5f;
-	truck_center_Y = 0.5f;
-	truck_center_Z = 0;
+	initial_camera_Y = 0.6;
+	initial_camera_Z = 0.9f;
 
 	glm::ivec2 resolution = window->GetResolution();
-	miniViewportArea = ViewportArea(50, 50, resolution.x / 5.f, resolution.y / 5.f);
+	miniViewportArea = ViewportArea(50, 50, resolution.x / 3.f, resolution.y / 3.f);
 }
 
 void Tema2::MyRenderMesh(Mesh* mesh, Shader* shader, const glm::mat4& modelMatrix)
@@ -302,19 +317,15 @@ void Tema2::update_camera() {
 	camera->Set(glm::vec3(initial_camera_X, initial_camera_Y, initial_camera_Z), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 	camera->MoveForward(-translateZ);
 	camera->TranslateRight(translateX);
-	camera->TranslateRight(0.5);
-	camera->MoveForward(-truck_center_Z + initial_camera_Z);
+	camera->MoveForward(initial_camera_Z);
 	camera->RotateFirstPerson_OY(rotation_angle_OY);
-	camera->MoveForward(+truck_center_Z - initial_camera_Z);
+	camera->MoveForward(-initial_camera_Z);
 }
 
 void Tema2::update_truck() {
 	main_transform = glm::mat4(1);
 	main_transform *= transform3D::Translate(translateX, translateY, translateZ);
-	main_transform *= transform3D::Translate(0.5, 0, 0);
 	main_transform *= transform3D::RotateOY(rotation_angle_OY);
-	main_transform *= transform3D::Translate(-0.5, 0, 0);
-
 	MyRenderMesh(meshes["truck"], shaders["VertexColor"], main_transform);
 }
 
@@ -326,6 +337,7 @@ void Tema2::render_ground() {
 void Tema2::Update(float deltaTimeSeconds)
 {
 	projectionMatrix = glm::perspective(RADIANS(80), window->props.aspectRatio, 0.01f, 200.0f);
+	//DrawCoordinateSystem(camera->GetViewMatrix(), projectionMatrix);
 	update_camera();
 	render_road();
 	render_trees();
@@ -334,7 +346,7 @@ void Tema2::Update(float deltaTimeSeconds)
 
 	glClear(GL_DEPTH_BUFFER_BIT);
 	glViewport(miniViewportArea.x, miniViewportArea.y, miniViewportArea.width, miniViewportArea.height);
-	camera->Set(glm::vec3(translateX, 20, translateZ), glm::vec3(translateX, 0, translateZ), glm::vec3(-1, 0, -1));
+	camera->Set(glm::vec3(translateX, 6, translateZ), glm::vec3(translateX, 0, translateZ), glm::vec3(-1, 0, -1));
 
 	render_road();
 	render_trees();
@@ -353,7 +365,7 @@ void Tema2::OnInputUpdate(float deltaTime, int mods)
 		{
 			new_translate_Z += truck_speed * cos(rotation_angle_OY) * deltaTime;
 			new_translate_X += truck_speed * sin(rotation_angle_OY) * deltaTime;
-			if (check_on_road(new_translate_X + 0.5, new_translate_Z)) {
+			if (check_on_road(new_translate_X, new_translate_Z)) {
 				translateX = new_translate_X;
 				translateZ = new_translate_Z;
 			}
@@ -362,7 +374,7 @@ void Tema2::OnInputUpdate(float deltaTime, int mods)
 		{
 			new_translate_Z -= truck_speed * cos(rotation_angle_OY) * deltaTime;
 			new_translate_X -= truck_speed * sin(rotation_angle_OY) * deltaTime;
-			if (check_on_road(new_translate_X + 0.5, new_translate_Z)) {
+			if (check_on_road(new_translate_X, new_translate_Z)) {
 				translateX = new_translate_X;
 				translateZ = new_translate_Z;
 			}
